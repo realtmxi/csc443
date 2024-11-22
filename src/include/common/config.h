9fa50a54016be021c76bfc8ccc4f56fd7f994ccd
Murphy <murphytian06@gmail.com>
2024-11-22 23:46:40 +0800

    update common

---
+++ src/include/common/config.h
@@ -0,0 +1,9 @@
+#include <cstdint>
+
+using frame_id_t = int32_t; // frame id type
+using page_id_t = int32_t; // page id type
+
+static constexpr int BUFFER_POOL_SIZE = 128; // size of buffer pool
+static constexpr int PAGE_SIZE = 4096; // size of data page in byte
+static constexpr int BUCKET_SIZE = 50; // size of extendible hash bucket6
+
--- src/include/utils/config.h
+++
@@ -1,2 +0,0 @@
-
-typedef frame_id_t = int32_t; // frame id type
